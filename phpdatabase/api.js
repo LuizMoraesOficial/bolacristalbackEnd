7df4652693cf1a98f82beaf34777e332beec830d
LuizMoraesOficial <luizmoraesofc@gmail.com>
2025-10-14 07:05:17 -0300

    Update api.js

--- phpdatabase/api.js
+++ phpdatabase/api.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://localhost/cartomante/phpdatabase";
+const API_BASE = "https://bolacristalbackend-1.onrender.com";
 
 export async function getPosts() {
   const res = await fetch(`${API_BASE}/getPosts.php`);
